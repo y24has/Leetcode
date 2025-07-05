d5de1877666bded37bd9fd73b839b699740aa142
Yashas S Narayan <159248629+y24has@users.noreply.github.com>
2025-07-05 21:38:37 +0530

    Time: 25 ms (7.23%), Space: 45.1 MB (6.77%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,18 +1,18 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-    HashMap<Character,Integer> freq_s=new HashMap<>();
-    HashMap<Character,Integer> freq_t=new HashMap<>();    
-      s=s.toLowerCase();
-      t=t.toLowerCase();
-      for(char ch: s.toCharArray()){
-       freq_s.put(ch,freq_s.getOrDefault(ch,0)+1);
-      }
+       if (s.length() != t.length()) return false;
+        Map<Character,Integer> s_map=new HashMap<>();
+        Map<Character,Integer> t_map=new HashMap<>();
+
+      
+for(int i=0;i<s.length();i++){
+    char  ch_s= s.charAt(i);
+    s_map.put(ch_s, s_map.getOrDefault(ch_s, 0) + 1);   
+    char  ch_t =t.charAt(i);
+    t_map.put(ch_t, t_map.getOrDefault(ch_t, 0) + 1);
+}
 
 
-      for(char ch:t.toCharArray()){
-       freq_t.put(ch,freq_t.getOrDefault(ch,0)+1);
-      }
-
-       return (freq_s.equals(freq_t))?true:false;
+return s_map.equals(t_map);
     }
 }
