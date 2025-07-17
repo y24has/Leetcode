c54d61df55a1180020d4759d602a142e29eb078e
Yashas S Narayan <159248629+y24has@users.noreply.github.com>
2025-07-18 00:29:32 +0530

    Time: 2 ms (71.34%) | Memory: 46.5 MB (34.10%) - LeetSync

---
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        int n=graph.length;
+
+        int[] visited=new int[n];
+        List<Integer> path=new ArrayList<>();
+        List<List<Integer>> result=new ArrayList<>();
+        dfs(0,graph,visited,path,result);
+        return result;
+        
+    }
+    private void dfs(int source,int[][] graph,int[] visited,List<Integer> path,List<List<Integer>> result){
+        visited[source]=1;
+        path.add(source);
+        if(source==graph.length-1){
+            result.add(new ArrayList<>(path));                            
+        }else{
+            for(int neighbor:graph[source]){
+                if(visited[neighbor]==0){
+                     dfs(neighbor,graph,visited,path,result);
+                }
+            }
+        }
+        visited[source]=0;
+        path.remove(path.size() - 1);
+    }
+}
