d7fdb40693e82876b59fb0924d1fe9c7c8b9e96b
Yashas S Narayan <159248629+y24has@users.noreply.github.com>
2025-07-26 23:59:38 +0530

    Time: 10 ms (81.03%) | Memory: 49.7 MB (5.42%) - LeetSync

---
+++ 744-network-delay-time/network-delay-time.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int networkDelayTime(int[][] times, int n, int k) {
+        List<List<int[]>> adj=new ArrayList<>();
+        
+        for(int i=0;i<=n;i++) adj.add(new ArrayList<>());
+
+        for(int[] edge:times){
+            int source=edge[0],target=edge[1],weight=edge[2];
+            adj.get(source).add(new int[]{target,weight});
+        }
+        int N=n+1;
+       int[] dist=new int[n+1];
+       PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+       Arrays.fill(dist,Integer.MAX_VALUE);
+       dist[k]=0;
+        pq.add(new int[]{0,k});
+        while(!pq.isEmpty()){
+            int[] curr=pq.poll();
+            int source=curr[1], weight=curr[0];
+           
+            for(int[] neighbor:adj.get(source)){
+                int node=neighbor[0];
+                int time=neighbor[1];
+                if(dist[node]>weight+time){
+                    dist[node]=weight+time;
+                    pq.add(new int[]{dist[node],node});
+                }
+            }
+        }
+    for (int i = 1; i <= n; i++) {
+    if (dist[i] == Integer.MAX_VALUE) return -1;
+}
+    int max=0;
+    for(int i=1;i<=n;i++){
+           max=Math.max(dist[i],max);
+        }
+      return max;
+    }
+}
