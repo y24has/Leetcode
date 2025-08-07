38e56364e12c4060be522fc98939b5fcce543fd9
Yashas S Narayan <159248629+y24has@users.noreply.github.com>
2025-08-07 12:04:01 +0530

    Time: 3 ms (60.02%), Space: 54.1 MB (47.29%) - LeetHub

--- 1039-find-the-town-judge/1039-find-the-town-judge.java
+++ 1039-find-the-town-judge/1039-find-the-town-judge.java
@@ -1,13 +1,11 @@
 class Solution {
     public int findJudge(int n, int[][] trust) {
-        List<List<Integer>> adj=new ArrayList<>();
 
-        for(int i=0;i<=n;i++) adj.add(new ArrayList<>());
+    
         int[] indegree=new int[n+1];
-         int[] outdegree=new int[n+1];
+        int[] outdegree=new int[n+1];
         for(int[] edge:trust){
         int source=edge[0],target=edge[1];
-            adj.get(source).add(target);
         indegree[target]++;
         outdegree[source]++;
         }
