5b95d0245da9ccb095dae01c04bf8295c1482c00
Yashas S Narayan <159248629+y24has@users.noreply.github.com>
2025-08-13 16:36:36 +0530

    Time: 459 ms (33.00%) | Memory: 108.5 MB (71.00%) - LeetSync

---
+++ 3628-find-minimum-time-to-reach-last-room-ii/find-minimum-time-to-reach-last-room-ii.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int minTimeToReach(int[][] moveTime) {
+        int m = moveTime.length;
+        int n = moveTime[0].length;
+
+        int[][] result = new int[m][n];
+        for (int[] row : result) {
+            Arrays.fill(row, Integer.MAX_VALUE);
+        }
+
+        // PriorityQueue to hold entries as (time, i, j)
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
+        result[0][0] = 0;
+        pq.offer(new int[]{0, 0, 0, 0});
+        int[] drow = {-1, 0, 1, 0};
+        int[] dcol = {0, 1, 0, -1};
+        int wait=0;
+        while (!pq.isEmpty()) {
+            int[] curr = pq.poll();
+            int currTime = curr[0];
+            int i = curr[1];
+            int j = curr[2];
+            int parity=curr[3];
+            
+            for (int o=0;o<4;o++) {
+                int i_ = i + drow[o];
+                int j_ = j + dcol[o];
+                 if (i_ >= 0 && i_ < m && j_ >= 0 && j_ < n) {
+                  int arrTime=currTime;
+                   if(currTime<moveTime[i_][j_]){
+                     wait = Math.abs(moveTime[i_][j_] - currTime);
+                     arrTime+=wait;
+                   }
+                    int moveCost = (parity == 0 ? 1 : 2);
+                    arrTime += moveCost;
+                    int nextParity = 1 - parity;
+                    
+                    if (result[i_][j_] > arrTime) {
+                        result[i_][j_] = arrTime;
+                        pq.offer(new int[]{arrTime, i_, j_,nextParity});
+                    }
+                }
+            }
+        }
+    return result[m-1][n-1]; 
+    }
+}
