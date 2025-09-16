ab615872a956c003ea1f1103ddb3dbca015886d8
Yashas S Narayan <159248629+y24has@users.noreply.github.com>
2025-09-16 18:57:42 +0530

    Time: 76 ms (33.81%) | Memory: 58 MB (83.05%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.java
+++ 739-daily-temperatures/daily-temperatures.java
@@ -6,27 +6,18 @@ class Solution {
         ArrayList<Integer> res=new ArrayList<>();
         for(int i=n-1;i>=0;i--){
             
-            if(s.isEmpty()){
-                res.add(0);
-            }else if(!s.isEmpty() && temperatures[s.peek()]>temperatures[i]){
-                  res.add(Math.abs(s.peek()-i));
-            }
-            else{
                 while(!s.isEmpty() && temperatures[s.peek()]<=temperatures[i]){
                     s.pop();
                 }
                 if(s.isEmpty()){
-                    res.add(0);
+                    result[i]=0;
                 }else{
-                    res.add(Math.abs(s.peek()-i));
+                    result[i]=s.peek()-i;
                 }
-            }
             s.add(i);
         }
-        Collections.reverse(res);
-        for (int i =0;i<n;i++) {
-           result[i] = res.get(i);
-      }
+      
+     
         return result;
     }
 }
