39b0089f83c54e4d97d15fe0c1065c3a9d44ddcd
Yashas S Narayan <159248629+y24has@users.noreply.github.com>
2025-10-12 01:57:54 +0530

    Time: 23 ms (92.64%) | Memory: 46.4 MB (76.38%) - LeetSync

---
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int count = 0,sum=0;           
+        int n = nums.length;
+        Map<Integer,Integer> map=new HashMap<>();
+        map.put(0,1); // this coz [1,-1] and k=0 , we shd store a zero has first step as we are storing the cumulativesum in the map and cumulative sum starts from 0 we are doing tht  
+        for (int i = 0; i < n; i++) {
+            sum+=nums[i]; 
+            if(map.containsKey(sum-k)) {
+                count+=map.get(sum-k); 
+            }   
+             map.put(sum,map.getOrDefault(sum,0)+1);
+            }
+        return count;
+    }
+}
