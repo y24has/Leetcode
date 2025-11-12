e0fc3b734020dcb85ae5df0b733eee0fbe7bb905
Yashas S Narayan <159248629+y24has@users.noreply.github.com>
2025-11-12 22:44:37 +0530

    Time: 26 ms (23.68%), Space: 47.9 MB (7.43%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.java
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.java
@@ -1,28 +1,30 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-       long low=1;
+       long low=-1;
        long high=0;
        for(int pile:piles){
         high=Math.max(pile,high);
        } 
+        high+=1;
 
-
-       while(low<high){
+       while(low+1<high){
         long mid=low+(high-low)/2;
-        
+
         if(canFinish(piles,mid,h)){
             high=mid;
         }else{
-            low=mid+1;
+            low=mid;
         }
        }
-       return (int)low;
+       return (int)high;
     }
 
     static boolean canFinish(int[] piles,long mid,int h){
+        if (mid <= 0) return false;
         long count=0;
         for(int pile:piles){
-           count+=(pile+mid-1)/mid;
+        count += Math.ceil((double)pile / mid);
+
            if(count>h) return false;
         }
         return count<=h;
