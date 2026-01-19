a504d5fe01e51b2a4f69892c86b23ccb022f4ed8
Yashas S Narayan <159248629+y24has@users.noreply.github.com>
2026-01-19 13:19:53 +0530

    Time: 2 ms (11.55%) | Memory: 43 MB (28.75%) - LeetSync

---
+++ 1252-break-a-palindrome/break-a-palindrome.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String breakPalindrome(String s) {
+        int n=s.length();
+        if(n==1) return "";
+        for(int i=0;i<n/2;i++){
+           if(s.charAt(i)!='a'){
+            return s.substring(0,i)+'a'+s.substring(i+1);
+           }
+        }
+        return s.substring(0,n-1)+'b';
+    }
+}
