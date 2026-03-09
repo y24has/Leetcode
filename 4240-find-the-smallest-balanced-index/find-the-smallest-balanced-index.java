0a0cb287e13bb44ab218026a3f2fa8a79ff5feb1
Yashas S Narayan <159248629+y24has@users.noreply.github.com>
2026-03-10 01:22:03 +0530

    Time: 4 ms (91.81%) | Memory: 147 MB (16.67%) - LeetSync

---
+++ 4240-find-the-smallest-balanced-index/find-the-smallest-balanced-index.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int smallestBalancedIndex(int[] nums) {
+        long[] prefix_sum=new long[nums.length];
+
+
+        prefix_sum[0]=nums[0];
+        for(int j=1;j<nums.length;j++)
+        {
+             prefix_sum[j]=prefix_sum[j-1]+nums[j];
+        }
+        long product=1l;
+          int index=-1;
+        for(int i=nums.length-1;i>0;i--){
+            if(prefix_sum[i-1]==product){
+                index=i;
+            }
+            // prevent overflow / unnecessary huge values
+            if(product >prefix_sum[i]) {
+             return index; 
+            }
+            else{
+              product = product * nums[i];
+        }
+      }
+    return index;
+    }
+}
