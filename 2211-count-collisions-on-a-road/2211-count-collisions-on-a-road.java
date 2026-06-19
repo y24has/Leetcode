cb3d55af866aca6f7bb9eebabde0542b5a1b450f
Yashas S Narayan <159248629+y24has@users.noreply.github.com>
2026-06-19 10:36:10 +0530

    Time: 9 ms (96.72%), Space: 47.6 MB (59.41%) - LeetHub

--- 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.java
+++ 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.java
@@ -1,21 +1,68 @@
 class Solution {
-    public int countCollisions(String s) {
-    int n = s.length();
-    int left = 0, right = n - 1;
+    public int countCollisions(String directions) {
+//     int n = s.length();
+//     int left = 0, right = n - 1;
 
-    // 1. skip leading L
-    while (left < n && s.charAt(left) == 'L') left++;
+//     // 1. skip leading L
+//     while (left < n && s.charAt(left) == 'L') left++;
 
-    // 2. skip trailing R
-    while (right >= 0 && s.charAt(right) == 'R') right--;
+//     // 2. skip trailing R
+//     while (right >= 0 && s.charAt(right) == 'R') right--;
 
-    // 3. no collisions possible
-    if (left > right) return 0;
-    int collisions = 0;
-    for (int i = left; i <= right; i++) {
-        char curr = s.charAt(i);
-        if(curr!='S') collisions++;
-    }
-    return collisions;
+//     // 3. no collisions possible
+//   Stack<Character> st = new Stack<>();
+//     int collisions = 0;
+
+//     // 4. push the first active cow
+//     st.push(s.charAt(left));
+
+//     // 5. iterate over the middle
+//     for (int i = left + 1; i <= right; i++) {
+//         char curr = s.charAt(i);
+
+//         char top = st.peek();
+
+//         if (top == curr) {
+//             // same direction → no collision
+//             st.push(curr);
+//         } 
+//         else {
+//             // collision zone
+//             if (curr == 'S') {
+//                 collisions += 1;
+//                 break;
+//             } else {
+//                 // curr == R or L opposite to top
+//                 collisions += 2;
+//                 break;
+//             }
+//           //  st.clear();
+//            // st.push(curr);
+//         }
+//     }
+//     int add=n-left;
+//     return collisions+add;
+int left = 0;
+        int right = directions.length() - 1;
+        
+        // 1. Skip safe cars driving away to the Left
+        while (left <= right && directions.charAt(left) == 'L') {
+            left++;
+        }
+        
+        // 2. Skip safe cars driving away to the Right
+        while (left <= right && directions.charAt(right) == 'R') {
+            right--;
+        }
+        
+        // 3. Count all doomed moving cars in the middle
+        int collisions = 0;
+        for (int i = left; i <= right; i++) {
+            if (directions.charAt(i) != 'S') { // If it's L or R, it crashes
+                collisions++;
+            }
+        }
+        
+        return collisions;
     }
 }
